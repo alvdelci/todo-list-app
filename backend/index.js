f46a8e862e1e9d9ee502fa229e829a18e3078ca5
alvdelci <rafithydev@gmail.com>
2022-08-26 22:01:06 -0300

    feat(server): Configurando servidor

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-import express from 'express';
+const express = require("express");
+const routes = require("./routes/routes");
+
+const app = express();
+const port = 3333;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use("/", routes);
+
+app.listen(port, () => {
+    console.log("App online on port", port);
+});
