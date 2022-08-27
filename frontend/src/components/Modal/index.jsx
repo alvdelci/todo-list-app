c2971faf06f413ce82adf3c742493b8ddcde3fe7
alvdelci <rafithydev@gmail.com>
2022-08-27 06:18:20 -0300

    feat: Adicionando modal de cadastro

--- frontend/src/components/Modal/index.jsx
+++ frontend/src/components/Modal/index.jsx
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, CloseSection, Modal, Title } from "./styles";
 
 
 export default function ModalComponent(props) {
 
+    const initialState = {
+        name: null,
+        description: null
+    };
+
+    const [data, setData] = useState(initialState);
+
+
     return (<>
         <CloseSection show={props.show} onClick={props.handleClose} />
         <Modal show={props.show}>
             <Title>Adicionar Nota Tarefa</Title>
-            <div className="inputs">
-                <label>Título da Tarefa</label><br />
-                <input type="text" placeholder="Ex: Alimentar o pet" /><br />
+            <form>
+                <div className="inputs">
+                    <label>Título da Tarefa</label><br />
+                    <input name="name" type="text" placeholder="Ex: Alimentar o pet" /><br />
 
-                <label>Descrição da Tarefa</label><br />
-                <textarea placeholder="Ex: O pet não come whiskas" />
-            </div>
+                    <label>Descrição da Tarefa</label><br />
+                    <textarea name="description" placeholder="Ex: O pet não come whiskas" />
+                </div>
 
-            <div className="buttons">
-                <Button onClick={props.saveData} color="green">Salvar</Button>
-                <Button onClick={props.cancelAction} color="orange">Cancelar</Button>
-            </div>
+                <div className="buttons">
+                    <Button type="submit" onClick={() => { }} color="green">Salvar</Button>
+                    <Button onClick={() => props.cancelAction(initialState)} color="orange">Cancelar</Button>
+                </div>
+            </form>
         </Modal>
     </>);
 
