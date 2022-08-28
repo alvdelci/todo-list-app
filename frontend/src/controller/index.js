d434893f35910f4aaa5ac48e1201fecc53e0eec8
alvdelci <rafithydev@gmail.com>
2022-08-27 21:04:34 -0300

    feat: Adicionando função de busca

--- frontend/src/controller/index.js
+++ frontend/src/controller/index.js
@@ -8,11 +8,17 @@ export function update(params) {
     api.post("/update", params);
 }
 
-export function remove(params) {
-    api.post("/delete", params);
+export async function remove(params) {
+    let res = await api.post("/delete", params);
+    return res;
 }
 
 export async function view() {
     let res = await api.get("/view");
+    return res;
+}
+
+export async function find(params) {
+    let res = await api.post("/find", params);
     return res;
 }
